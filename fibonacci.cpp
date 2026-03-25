2d42017a1c5fdcdc8668772b94457d1bd25c52fb
krishang verma <92907548+krishang13@users.noreply.github.com>
2026-03-25 13:56:27 +0530

    fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10, a = 0, b = 1, next;
+
+    cout << "Fibonacci Series: ";
+    for(int i = 0; i < n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return 0;
+}
